67fa82d4e39d3607669baf4471e3413bde023b73
Karl Isenberg <karl@mesosphere.io>
2018-01-18 16:41:07 -0800

    Fix versioning to drop ~ce

--- image-version.sh
+++ image-version.sh
@@ -12,7 +12,7 @@ wrapper_version=$(docker run --rm --entrypoint bash "${image}" -c 'echo ${WRAPPE
 mesos_version=$(docker run --rm --entrypoint bash "${image}" -c "mesos-slave --version | tr -s ' ' | cut -d ' ' -f 2")
 
 # Get docker version from DOCKER_VERSION env variable
-docker_version=$(docker run --rm --entrypoint bash "${image}" -c 'echo "${DOCKER_VERSION}" | cut -d "-" -f 1')
+docker_version=$(docker run --rm --entrypoint bash "${image}" -c 'echo "${DOCKER_VERSION}" | cut -d "-" -f 1 | cut -d "~" -f 1')
 
 # Get ubuntu version (with patch)
 ubuntu_version=$(docker run --rm --entrypoint bash "${image}" -c "lsb_release -sd |  tr -s ' ' | cut -d ' ' -f 2")
